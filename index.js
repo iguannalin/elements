d4442d5578e09705f946a15103cb1509fe7a8085
seasons986 <annalin12345@gmail.com>
2023-10-11 02:11:36 -0400

    init

---
+++ index.js
@@ -0,0 +1,22 @@
+window.addEventListener("load", () => {
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+  }
+
+  const container = document.getElementById("container");
+  // window.addEventListener("keypress", (k) => {
+  //   const kbd = document.createElement("kbd");
+  //   console.log(k);
+  //   kbd.innerText = k.keyCode;
+  //   container.appendChild(kbd);
+  // })
+  fetch("elements.json").then((r) => r.json()).then((d) => {
+    Array.from(d).forEach((elem) => {
+      const el = document.createElement(elem);
+      el.innerText = "quick fox";
+      container.appendChild(el);
+    })
+  })
+});
